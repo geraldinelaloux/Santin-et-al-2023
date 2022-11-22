ea49d2e175bf7d304612a6d5ad4eaa3957908839
adminuser <adminuser@PC-114-178.OASIS.UCLOUVAIN.BE>
2022-11-22 15:21:21 +0100

    * Switched functions; added comments

--- Pipeline/readers.r
+++ Pipeline/readers.r
@@ -1,3 +1,13 @@
+#################################################################
+# Thomas Lamot, 2022
+# Used in Santin et al, 2022
+
+## The functions below depend on the following functions in extract_from.R: extract_oufti_celllist(), extract_microbeJ_mesh() & extract_microbeJ_cell()
+
+##Dependencies: R.matlab, tidyverse
+
+##Oufti
+
 read_oufti <- function(oufti_matfile) {
   oufti_matfile |>
     R.matlab::readMat(oufti_matfile) |>
@@ -14,11 +24,32 @@ read_oufti_parameters <- function(oufti_matfile) {
   return(param)
 }
 
+##MicrobeJ
+
+read_microbeJ_mesh <- function(microbeJ_csv){
+  microbeJ_df <- read_csv(microbeJ_csv)
+  microbeJ_df |> 
+    extract_microbeJ_mesh() -> mesh
+  return(mesh)
+}
+
+read_microbeJ_cell <- function(microbeJ_csv){
+  microbeJ_df <- read_csv(microbeJ_csv) |>
+    extract_microbeJ_cell()
+  return(microbeJ_df)
+}
+
+##################################################################
+
+#Functions for other input programs; not tested in this context:
+
+#Supersegger
+
 read_supersegger_cell <- function(supersegger_matfile) {
   supersegger_matfile |> 
     R.matlab::readMat() |>
     extract_supersegger_celllist() -> cell_list
-
+  
   return(cell_list)
 }
 
@@ -43,23 +74,10 @@ read_supersegger_mesh <- function(supersegger_path) {
   stop("Invalid file format")
 }
 
+#Morphometrics
+
 read_morphometrics <- function(morphometrics_matfile){
   morphometrics_list <- R.matlab::readMat(morphometrics_matfile)
   cell_list <- extract_morphometrics(morphometrics_list)
   return(cell_list)
-}
-
-read_microbeJ_mesh <- function(microbeJ_csv){
-  microbeJ_df <- read_csv(microbeJ_csv)
-  microbeJ_df |> 
-    extract_microbeJ_mesh() -> mesh
-  return(mesh)
-}
-
-
-
-read_microbeJ_cell <- function(microbeJ_csv){
-  microbeJ_df <- read_csv(microbeJ_csv) |>
-    extract_microbeJ_cell()
-  return(microbeJ_df)
 }
